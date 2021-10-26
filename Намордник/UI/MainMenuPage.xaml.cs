9496a56b6279a87efc73e08f031e7d5546b18ad2
Сафронов Дмитрий Алексеевич <da_safronov@itlabs.univer.local>
2021-10-26 15:41:42 +0300

    Initial commit

---
+++ Намордник/UI/MainMenuPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Намордник.UI;
+
+namespace Намордник
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenuPage.xaml
+    /// </summary>
+    public partial class MainMenuPage : Page
+    {
+        public MainMenuPage()
+        {
+            InitializeComponent();
+
+            PageHelper.FrameName.Text = "Главное меню";
+        }
+
+        private void BtnToProductList_Click(object sender, RoutedEventArgs e)
+        {
+            PageHelper.MainFrame.Navigate(new ProductListPage());
+        }
+
+        private void BtnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            PageHelper.MainFrame.Navigate(new AddProductPage());
+        }
+    }
+}
---
+++ Намордник/UI/ProductListPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Намордник.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListPage.xaml
+    /// </summary>
+    public partial class ProductListPage : Page
+    {
+        public ProductListPage()
+        {
+            InitializeComponent();
+
+            DbGrid.ItemsSource = PageHelper.Entities.Product.ToList();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            PageHelper.MainFrame.GoBack(); // Navigate(new MainMenuPage())
+        }
+    }
+}
